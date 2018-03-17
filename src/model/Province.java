cc66c656bcecd3ad81794d3cf92f112463f30b9f
Tony <370732726@qq.com>
2018-03-17 22:58:00 +0800

    new database help.

---
+++ src/model/Province.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Province {
+     private int id;
+     private String provinceName;
+     private String provinceCode;
+     
+     public int getId() {
+		return id;
+     }
+     
+     public void setId(int id) {
+    	 this.id = id;
+     }
+     
+     public String getProvinceName() {
+		return provinceName;
+    	 
+     }
+     
+     public void setProvinceCode(String provinceName) {
+    	 this.provinceName =  provinceName;
+     }  
+     
+     public String getProvinceCode() {
+		return provinceCode;
+    	 
+     }
+     
+     public void setProvinceName(String provinceCode) {
+    	 this.provinceCode =  provinceCode;
+     }     
+     
+     
+     
+     
+     
+}
